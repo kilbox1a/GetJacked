0cfaf7380313277bc515c06376faf7adb041d809
c <gc@injijiwaterbottle.33mail.com>
2019-02-07 04:11:46 -0600

    channels
    changed channel  amounts

--- get-jacked.sh
+++ get-jacked.sh
@@ -9,14 +9,14 @@ pacmd unload-module module-jack-sink
 pacmd unload-module module-jack-source
 
 # configure pulse connectors
-pacmd load-module module-jack-source channels=1 source_name=rhythmbox-in client_name=rhythmbox-in connect=false
-pacmd load-module module-jack-sink channels=1 sink_name=rhythmbox-out client_name=rhythmbox-out connect=false
+pacmd load-module module-jack-source channels=2 source_name=rhythmbox-in client_name=rhythmbox-in connect=false
+pacmd load-module module-jack-sink channels=2 sink_name=rhythmbox-out client_name=rhythmbox-out connect=false
 
-pacmd load-module module-jack-source channels=1 source_name=firefox-in client_name=firefox-in connect=false
-pacmd load-module module-jack-sink channels=1 sink_name=firefox-out client_name=firefox-out connect=false
+pacmd load-module module-jack-source channels=2 source_name=firefox-in client_name=firefox-in connect=false
+pacmd load-module module-jack-sink channels=2 sink_name=firefox-out client_name=firefox-out connect=false
 
-pacmd load-module module-jack-source channels=1 source_name=spotify-in client_name=spotify-in connect=false
-pacmd load-module module-jack-sink channels=1 sink_name=spotify-out client_name=spotify-out connect=false
+pacmd load-module module-jack-source channels=2 source_name=spotify-in client_name=spotify-in connect=false
+pacmd load-module module-jack-sink channels=2 sink_name=spotify-out client_name=spotify-out connect=false
 
 # launch pulse applications
 PULSE_SINK=rhythmbox-out  &
